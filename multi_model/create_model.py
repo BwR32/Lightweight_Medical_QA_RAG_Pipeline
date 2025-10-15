913b9a69e9eb70ed5e7bc65a95a8a69c7d6341dd
BwR32 <149702062+BwR32@users.noreply.github.com>
2025-10-15 15:56:31 +0800

    Migrate all large files to LFS and finalize project structure

---
+++ multi_model/create_model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:35792d533a390483cd5dc170bc5be9044a9dae537860b1835ccbb1e8550805cf
+size 5509
---
+++ multi_model/evaluate_rag_versions.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b72ac5b1786a4bb3d5fe6bd33787eba711a89194bd33d36c7a03e717d3ed6f01
+size 8817
